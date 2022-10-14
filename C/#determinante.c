4b0ff963ba7ee2c10d6a2a47ca6fd47da35f3392
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-14 01:36:21 -0300

    Update #determinante.c

--- C/#determinante.c
+++ C/#determinante.c
@@ -1,15 +1,26 @@
-#include <stdio.h>
-int determinante(int *vetor,int tamanho){
-    return 0;
-}
-void main(){
-    int a[2][2];
-    a[0][0]=1;
-    a[0][1]=2;
-    a[1][0]=10;
-    a[1][1]=11;
-    int *b;
-    b=&a[0][0];
-    b=b+3;
-    printf("%d",*b);
+int determinante(int *matriz,int tamanho){
+    if(tamanho == 1){
+        return *matriz;
+    }
+    int i,j,psm,psb,det_soma,det_sub,det=0;
+    for(i=0;i<tamanho-1;i++){
+        psm=i;
+        psb=(tamanho-1)-i;
+        det_soma=1;
+        det_sub=1;
+        for(j=0;j<tamanho;j++){
+            det_soma*=*(matriz+j+(psm*tamanho));
+            det_sub*=*(matriz+j+(psb*tamanho));
+            psm+=1;
+            psb-=1;
+            if(psm == tamanho){
+                psm=0;
+            }
+            if(psb == -1){
+                psb=tamanho-1;
+            }
+        }
+        det+=(det_soma-det_sub);
+    }
+    return det;
 }
