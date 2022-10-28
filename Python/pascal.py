51e6f8de1ff4e8affb634eb22ab8f0064de1bc15
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-27 22:28:42 -0300

    Update pascal.py

--- Python/pascal.py
+++ Python/pascal.py
@@ -1,24 +1,18 @@
 def pascal(n=1):
-    linha1=[1]
-    linha2=[1,1]
-    if n==0:
-        return [1]
-    elif n == 1:
-        return [1,1]
-    retorno=linha2
+    linhas=[[1],[1,1]]#inicializa a primeira linha e a segunda linha
+    retorno=linhas[n%2]
+    
+    #calcula os valores de cada linha
     for i in range(2,n+1):
-        if i%2==0:
-            linha2=[1]
-            for j in range(len(linha1)-1):
-                linha2.append(linha1[j]+linha1[j+1])
-            linha2.append(1)
-            retorno=linha2
-        else:
-            linha1=[1]
-            for j in range(len(linha2)-1):
-                linha1.append(linha2[j]+linha2[j+1])
-            linha1.append(1)
-            retorno=linha1
+        #calcula os elementos de cada linha de acordo com par ou impar
+        linhas[i%2]=[1]#primeiro elemento da linha
+        
+        for j in range(len(linhas[(i+1)%2])-1):
+            #elemento j+1 é a soma dos elementos j e j-1 da linha anterior
+            linhas[i%2].append(linhas[(i+1)%2][j]+linhas[(i+1)%2][j+1])
+            
+        linhas[i%2].append(1)#ultimo elemento da linha
+        retorno=linhas[i%2]
     return retorno
     
 print(pascal(int(input("Digite a linha desejada:"))))
